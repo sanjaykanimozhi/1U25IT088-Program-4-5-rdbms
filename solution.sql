1db8f4a642bb930219710b86b73a72e6f9c2e2ea
Sanjay <sanjaykanimozhi58@gmail.com>
2026-08-22 10:15:53 +0530

    PROGRAM 4 & 5 RDBMS

--- solution.sql
+++ solution.sql
@@ -1,22 +1,20 @@
-CREATE DATABASE CollegeDB;
 USE CollegeDB;
 
--- Program 4
-CREATE TABLE Student (
-    StudentID NUMERIC(10),
-    StudentName VARCHAR(10),
-    Gender VARCHAR(10),
-    DepartmentID NUMERIC(10)
-);
+-- Insert records into existing Student table
+INSERT INTO Student
+VALUES (1001, 'Arun', 'Male', 101);
 
-INSERT INTO Student VALUES (1001, 'Arun', 'Male', 101);
-INSERT INTO Student VALUES (1002, 'Divya', 'Female', 102);
-INSERT INTO Student VALUES (1003, 'Karthik', 'Male', 101);
+INSERT INTO Student
+VALUES (1002, 'Divya', 'Female', 102);
 
+INSERT INTO Student
+VALUES (1003, 'Karthik', 'Male', 101);
+
+-- Display all Student records
 SELECT * FROM Student;
 
 
--- Program 5
+-- Create Course table
 CREATE TABLE Course (
     CourseID NUMERIC(10),
     CourseName VARCHAR(30),
@@ -24,9 +22,15 @@ CREATE TABLE Course (
     DepartmentID VARCHAR(30)
 );
 
-INSERT INTO Course VALUES (2025011, 'Information Technology', 95, 'BSC IT');
-INSERT INTO Course VALUES (2025002, 'Computer Science', 90, 'BSC CS');
-INSERT INTO Course VALUES (2025003, 'Information Technology', 85, 'BCA IT');
+-- Insert records into Course table
+INSERT INTO Course
+VALUES (2025011, 'Information Technology', 95, 'BSC IT');
 
-SELECT * FROM Course;
+INSERT INTO Course
+VALUES (2025002, 'Computer Science', 90, 'BSC CS');
 
+INSERT INTO Course
+VALUES (2025003, 'Information Technology', 85, 'BCA IT');
+
+-- Display Course table structure
+DESCRIBE Course;
